df305427ab32eb0d100cee61d74ba67b4ed599be
grzegorian <grzegorzpiasta@gmail.com>
2020-03-04 13:19:29 +0100

    + wyświetla już w prawidłowym oknie + poprawiona nazwa klasy - wciaz parasolka

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DriverAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DriverAdapter.java
@@ -4,34 +4,35 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
  */
-public class MyAdapter extends DrawPanelController implements Job2dDriver {
+public class DriverAdapter extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
-
-	public MyAdapter() {
+	
+	public DriverAdapter() {
 		super();
 	}
-
+	
 	@Override
 	public void setPosition(int x, int y) {
 		this.startX = x;
 		this.startY = y;
 	}
-
+	
 	@Override
 	public void operateTo(int x, int y) {
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
-
-		drawLine(line);
+		
+		DrawerFeature.getDrawerController().drawLine(line);
 	}
-
+	
 	@Override
 	public String toString() {
-		return "@Q!$!@$!#@$(*#@&Q(%^*#@";
+		return "DriverAdapter";
 	}
 }
