483621d2cfea655760b6db2ebb9d9e5df4600e3e
grzegorian <grzegorzpiasta@gmail.com>
2020-03-04 13:22:06 +0100

    + poprawiono rysowanie

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DriverAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DriverAdapter.java
@@ -27,7 +27,8 @@ public class DriverAdapter extends DrawPanelController implements Job2dDriver {
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
-		
+		startX=x;
+		startY=y;
 		DrawerFeature.getDrawerController().drawLine(line);
 	}
 	
