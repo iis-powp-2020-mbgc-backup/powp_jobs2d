b2d3fa2da9d5e54f6cce9a45e2fe651a11372696
Trackres <Trackres@users.noreply.github.com>
2020-03-10 09:55:01 +0100

    First Lesson

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
@@ -4,6 +4,8 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
+import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 /**
  * driver adapter to drawer with several bugs.
@@ -12,8 +14,11 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
 
 	public MyAdapter() {
+
 		super();
 	}
+
+
 
 	@Override
 	public void setPosition(int x, int y) {
