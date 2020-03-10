65a9974d5841d2a68b0a7e116a9ffe664378c3f6
uposledzony <chrustek123456789@gmail.com>
2020-03-10 09:57:19 +0100

    Trying to fix development level issues with new adapter.

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/Job2dDriverAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/Job2dDriverAdapter.java
@@ -5,14 +5,17 @@ import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
 
+import javax.swing.*;
+
 /**
  * driver adapter to drawer with several bugs.
  */
-public class MyAdapter extends DrawPanelController implements Job2dDriver {
+public class Job2dDriverAdapter implements Job2dDriver {
 	private int startX = 0, startY = 0;
+	private DrawPanelController drawPanelController;
 
-	public MyAdapter() {
-		super();
+	public Job2dDriverAdapter(DrawPanelController drawPanelController) {
+		this.drawPanelController = drawPanelController;
 	}
 
 	@Override
@@ -27,7 +30,8 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
 
-		drawLine(line);
+		drawPanelController.drawLine(line);
+		setPosition(x, y);
 	}
 
 	@Override
