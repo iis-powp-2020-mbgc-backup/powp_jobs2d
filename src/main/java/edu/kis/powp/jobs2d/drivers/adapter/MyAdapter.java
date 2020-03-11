81dc3b708921253e332998bf911440f180a3595a
Pawelllaz <pawelllaz@gmail.com>
2020-03-11 13:41:19 +0100

    update coordinates

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
@@ -27,6 +27,9 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
 
+		startX = x;
+		startY = y;
+
 		drawLine(line);
 	}
 
