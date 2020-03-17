9ce0f2d9e996222b6f55a4461dde28a995fa1cf9
Przemysław Frąg <215719@edu.p.lodz.pl>
2020-03-17 22:54:56 +0100

    Changed adapter class name

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawDriver.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawDriver.java
@@ -8,7 +8,7 @@ import edu.kis.powp.jobs2d.features.DrawerFeature;
 /**
  * driver adapter to drawer with several bugs.
  */
-public class MyAdapter implements Job2dDriver {
+public class DrawDriver implements Job2dDriver {
 	private int startX = 0, startY = 0;
 
 	@Override
