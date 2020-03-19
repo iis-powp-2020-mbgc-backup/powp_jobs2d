70830b23b277ccbe05ba983208a8d56648efff5e
Asmei <kamsykula@gmail.com>
2020-03-19 11:50:46 +0100

    Added FigureType to recognize type of draw example

---
+++ src/test/java/edu/kis/powp/jobs2d/FigureType.java
@@ -0,0 +1,6 @@
+package edu.kis.powp.jobs2d;
+
+public enum FigureType {
+	FIGURE_JOE_1,
+	FIGURE_JOE_2,
+}
--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -3,24 +3,27 @@ package edu.kis.powp.jobs2d.events;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import edu.kis.powp.jobs2d.FigureType;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigureOptionListener implements ActionListener {
 	
 	private DriverManager driverManager;
+	private FigureType figureType;
 	
-	public SelectTestFigureOptionListener(DriverManager driverManager) {
+	public SelectTestFigureOptionListener(DriverManager driverManager, FigureType figureType) {
 		this.driverManager = driverManager;
+		this.figureType = figureType;
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		switch (e.getActionCommand()) {
-			case "Figure Joe 1":
+		switch (figureType) {
+			case FIGURE_JOE_1:
 				FiguresJoe.figureScript1(driverManager.getCurrentDriver());
 				break;
-			case "Figure Joe 2":
+			case FIGURE_JOE_2:
 				FiguresJoe.figureScript2(driverManager.getCurrentDriver());
 				break;
 		}
