ec474c0fd842b2d8b958b89192e89e3c378e2638
rosiak.mateusz <215866@edu.p.lodz.pl>
2020-03-20 19:13:33 +0100

    Fix test to match new adapter constructor.

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -39,7 +39,8 @@ public class TestJobs2dPatterns {
 		DriverFeature.addDriver("Logger Driver", loggerDriver);
 		DriverFeature.getDriverManager().setCurrentDriver(loggerDriver);
 
-		Job2dDriver testDriver = new Jobs2dDriverToDrawerAdapter();
+		DrawPanelController drawPanelController = new DrawPanelController();
+		Job2dDriver testDriver = new Jobs2dDriverToDrawerAdapter(drawPanelController);
 		DriverFeature.addDriver("Buggy Simulator", testDriver);
 
 		DriverFeature.updateDriverInfo();
