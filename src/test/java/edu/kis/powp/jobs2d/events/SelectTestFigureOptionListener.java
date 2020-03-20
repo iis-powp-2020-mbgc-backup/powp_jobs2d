c09f3e73851973ff37f5b438773a08aae390965b
rosiak.mateusz <215866@edu.p.lodz.pl>
2020-03-20 19:48:26 +0100

    Code reformat and optimize imports

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -8,19 +8,19 @@ import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigureOptionListener implements ActionListener {
 
-	private DriverManager driverManager;
+    private DriverManager driverManager;
 
-	public SelectTestFigureOptionListener(DriverManager driverManager) {
-		this.driverManager = driverManager;
-	}
+    public SelectTestFigureOptionListener(DriverManager driverManager) {
+        this.driverManager = driverManager;
+    }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if(e.getActionCommand().toLowerCase().equals("figure joe 1")){
-			FiguresJoe.figureScript1(driverManager.getCurrentDriver());
-		} else if(e.getActionCommand().toLowerCase().equals("figure joe 2")) {
-			FiguresJoe.figureScript2(driverManager.getCurrentDriver());
-		}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().toLowerCase().equals("figure joe 1")) {
+            FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+        } else if (e.getActionCommand().toLowerCase().equals("figure joe 2")) {
+            FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+        }
 
-	}
+    }
 }
