45dcebc679ec01e1dfc056b05160930a49b16f3a
matlukomski94 <matlukomski94@gmail.com>
2020-03-24 17:56:33 +0100

    add LineDrawerAdapter

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -7,8 +7,10 @@ import java.util.logging.Logger;
 
 import edu.kis.legacy.drawer.panel.DefaultDrawerFrame;
 import edu.kis.legacy.drawer.panel.DrawPanelController;
+import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.appbase.Application;
 import edu.kis.powp.jobs2d.drivers.adapter.Jobs2DDriverAdapter;
+import edu.kis.powp.jobs2d.drivers.adapter.LineDrawerAdapter;
 import edu.kis.powp.jobs2d.events.SelectChangeVisibleOptionListener;
 import edu.kis.powp.jobs2d.events.SelectTestFigureOptionListener;
 import edu.kis.powp.jobs2d.features.DrawerFeature;
@@ -40,8 +42,10 @@ public class TestJobs2dPatterns {
 		DriverFeature.getDriverManager().setCurrentDriver(loggerDriver);
 
 		Job2dDriver testDriver = new Jobs2DDriverAdapter();
-		DriverFeature.addDriver("Buggy Simulator", testDriver);
+		DriverFeature.addDriver("Jobs2d Drawer Simulator", testDriver);
 
+		Job2dDriver testDriver2 = new LineDrawerAdapter(LineFactory.getSpecialLine(), DrawerFeature.getDrawerController());
+		DriverFeature.addDriver("Special Line Drawer Simulator", testDriver2);
 		DriverFeature.updateDriverInfo();
 	}
 
