a9b4dbf9e48be39a7270b881c061b57e967eba32
Graleszczur <gralewski.bartosz@gmail.com>
2020-03-25 13:57:57 +0100

    fix problem with window displaying (propably git reverted my changes

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -27,6 +27,7 @@ public class TestJobs2dPatterns {
                 DriverFeature.getDriverManager());
 
         application.addTest("Figure Joe 1", selectTestFigureOptionListener);
+
     }
 
     /**
@@ -83,8 +84,6 @@ public class TestJobs2dPatterns {
             public void run() {
                 Application app = new Application("2d jobs Visio");
                 DrawerFeature.setupDrawerPlugin(app);
-                setupDefaultDrawerVisibilityManagement(app);
-
                 DriverFeature.setupDriverPlugin(app);
                 setupDrivers(app);
                 setupPresetTests(app);
