8623c17a51e41e3e8ead70d1f7e3b4b6bae36425
Damian Jacewicz <215739@edu.p.lodz.pl>
2020-03-25 18:06:12 +0100

    changed adapter class name

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/JobDriverToDrawer.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/JobDriverToDrawer.java
@@ -9,10 +9,10 @@ import edu.kis.powp.jobs2d.features.DrawerFeature;
 /**
  * driver adapter to drawer with several bugs.
  */
-public class DrawControllerTo2dDriver extends DrawPanelController implements Job2dDriver {
+public class JobDriverToDrawer extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
 
-	public DrawControllerTo2dDriver() {
+	public JobDriverToDrawer() {
 		super();
 	}
 
