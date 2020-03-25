2b5eeae4e120b023832907e9efe542d8f4a619c3
Krzysztof Chybicki <krysiek97@interia.eu>
2020-03-25 13:20:10 +0100

    3.4 - Stowrzenie klasy dziedziczącej z AbstractDriver

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/AbstractDriverAdapter.java
@@ -0,0 +1,26 @@
+package edu.kis.powp.jobs2d.drivers.adapter;
+
+import edu.kis.legacy.drawer.shape.ILine;
+import edu.kis.legacy.drawer.shape.LineFactory;
+import edu.kis.powp.jobs2d.AbstractDriver;
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class AbstractDriverAdapter extends AbstractDriver {
+
+    private Job2dDriver job2dDriver;
+
+    public AbstractDriverAdapter(Job2dDriver job2dDriver) {
+        super(0, 0);
+
+        this.job2dDriver = job2dDriver;
+    }
+
+    @Override
+    public void operateTo(int x, int y) {
+        job2dDriver.setPosition(getX(), getY());
+
+        job2dDriver.operateTo(x, y);
+
+        setPosition(x, y);
+    }
+}
