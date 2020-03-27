6a2b7e05d309bc25698b7e8abfe215efa2d8ae97
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-03-27 22:22:24 +0100

    Added new type of line UserModifiableLine, that can be modified using method rebuilder() that returns LineBuilder object, moreover developed new factory of lines called SpecialLineFactory, this class includes introduced new type of line.

---
+++ src/main/java/edu/kis/powp/jobs2d/shapes/lines/UserModifiableLine.java
@@ -0,0 +1,62 @@
+package edu.kis.powp.jobs2d.shapes.lines;
+
+import edu.kis.legacy.drawer.shape.line.AbstractLine;
+
+import java.awt.*;
+
+public class UserModifiableLine extends AbstractLine {
+
+    private static final float DEFAULT_THICKNESS = 1.0f;
+    private static final Color DEFAULT_COLOR = Color.BLACK;
+    private static final boolean DEFAULT_STYLE = false;
+
+    private UserModifiableLine(LineBuilder builder) {
+        this.color = builder.color;
+        this.dotted = builder.dotted;
+        this.thickness = builder.thickness;
+    }
+
+    public UserModifiableLine() {
+        this.thickness = DEFAULT_THICKNESS;
+        this.color = DEFAULT_COLOR;
+        this.dotted = DEFAULT_STYLE;
+    }
+
+    public static final class LineBuilder {
+
+        private float thickness = DEFAULT_THICKNESS;
+        private Color color = DEFAULT_COLOR;
+        private boolean dotted = DEFAULT_STYLE;
+
+        private LineBuilder() {
+        }
+
+        public LineBuilder withThickness(float thickness) {
+            this.thickness = thickness;
+            return this;
+        }
+
+        public LineBuilder withColor(Color color) {
+            this.color = color;
+            return this;
+        }
+
+        public LineBuilder useDottedStyle(boolean value) {
+            this.dotted = value;
+            return this;
+        }
+
+        public UserModifiableLine build() {
+            return new UserModifiableLine(this);
+        }
+    }
+
+    public LineBuilder rebuilder() {
+        return new LineBuilder();
+    }
+
+    public static LineBuilder builder() {
+        return new LineBuilder();
+    }
+
+}
