34749b9e414ebcf22adb3697efe5f3d90c06f55d
DeanMiles <sevirion@gmail.com>
2020-03-30 22:17:23 +0200

    3.4

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -4,6 +4,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import edu.kis.powp.jobs2d.drivers.DriverManager;
+import edu.kis.powp.jobs2d.drivers.adapter.DriverAdapter;
+import edu.kis.powp.jobs2d.magicpresets.FiguresJane;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigureOptionListener implements ActionListener {
@@ -11,6 +13,7 @@ public class SelectTestFigureOptionListener implements ActionListener {
 	private DriverManager driverManager;
 	public static final String FIGURE_JOE_1 = "Figure Joe 1";
 	public static final String FIGURE_JOE_2 = "Figure Joe 2";
+	public static final String FIGURE_JANE_1 = "Figure Jane 1";
 
 	public SelectTestFigureOptionListener(DriverManager driverManager) {
 		this.driverManager = driverManager;
@@ -24,6 +27,10 @@ public class SelectTestFigureOptionListener implements ActionListener {
 				break;
 			case FIGURE_JOE_2:
 				FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+				break;
+			case FIGURE_JANE_1:
+				DriverAdapter driverAdapter = new DriverAdapter(driverManager.getCurrentDriver());
+				FiguresJane.figureScript(new DriverAdapter(driverAdapter));
 				break;
 			default:
 				System.out.println("Bad decision");
