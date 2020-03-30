b3291b4741d9ccd12d49d1d981337bed0cd499dc
Arek Dziuda <arekdziuda@gmail.com>
2020-03-30 16:07:50 +0200

    add DriverAdapter class

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DriverAdapter.java
@@ -0,0 +1,23 @@
+
+package edu.kis.powp.jobs2d.drivers.adapter;
+
+import edu.kis.powp.jobs2d.AbstractDriver;
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class DriverAdapter extends AbstractDriver {
+
+    private Job2dDriver job2dDriver;
+
+    public DriverAdapter(Job2dDriver job2dDriver) {
+        super(0, 0);
+        this.job2dDriver = job2dDriver;
+    }
+
+    @Override
+    public void operateTo(int x, int y) {
+        setPosition(x, y);
+
+        job2dDriver.setPosition(getX(), getY());
+        job2dDriver.operateTo(x, y);
+    }
+}
