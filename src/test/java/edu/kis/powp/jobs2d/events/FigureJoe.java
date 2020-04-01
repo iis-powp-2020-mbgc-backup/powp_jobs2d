688a89addf03ca4f0748e98a0ca18fad36aae370
Barabasz.Rafal <215671@edu.p.lodz.pl>
2020-04-01 13:49:12 +0200

    Added possibility to draw second shape

---
+++ src/test/java/edu/kis/powp/jobs2d/events/FigureJoe.java
@@ -0,0 +1,6 @@
+package edu.kis.powp.jobs2d.events;
+
+public enum FigureJoe {
+    SHAPE_1,
+    SHAPE_2
+}
--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -9,13 +9,21 @@ import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 public class SelectTestFigureOptionListener implements ActionListener {
 
 	private DriverManager driverManager;
+	private FigureJoe figureToDraw;
 
-	public SelectTestFigureOptionListener(DriverManager driverManager) {
+	public SelectTestFigureOptionListener(DriverManager driverManager, FigureJoe figureJoe) {
+		this.figureToDraw = figureJoe;
 		this.driverManager = driverManager;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+		switch (figureToDraw) {
+			case SHAPE_1:
+				FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+				break;
+			case SHAPE_2:
+				FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+		}
 	}
 }
