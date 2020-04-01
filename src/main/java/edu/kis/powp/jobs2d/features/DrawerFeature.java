f40dde1dd9f5caf06593a838d7952d85c8b7e24d
Patryk.Stepien <215886@edu.p.lodz.pl>
2020-04-01 14:39:30 +0200

    Added Special Line Simulator component and lines to menu

--- src/main/java/edu/kis/powp/jobs2d/features/DrawerFeature.java
+++ src/main/java/edu/kis/powp/jobs2d/features/DrawerFeature.java
@@ -2,6 +2,7 @@ package edu.kis.powp.jobs2d.features;
 
 import edu.kis.powp.appbase.Application;
 import edu.kis.powp.jobs2d.events.SelectClearPanelOptionListener;
+import edu.kis.powp.jobs2d.events.SelectLineTypeListener;
 import edu.kis.legacy.drawer.panel.DrawPanelController;
 
 public class DrawerFeature {
@@ -15,11 +16,15 @@ public class DrawerFeature {
 	 */
 	public static void setupDrawerPlugin(Application application) {
 		SelectClearPanelOptionListener selectClearPanelOptionListener = new SelectClearPanelOptionListener();
-
+		SelectLineTypeListener selectLineTypeListener = new SelectLineTypeListener();
+		
 		drawerController = new DrawPanelController();
 		application.addComponentMenu(DrawPanelController.class, "Draw Panel", 0);
 		application.addComponentMenuElement(DrawPanelController.class, "Clear Panel", selectClearPanelOptionListener);
-
+		application.addComponentMenuElement(DrawPanelController.class, "Basic line", selectLineTypeListener);
+		application.addComponentMenuElement(DrawPanelController.class, "Dotted line", selectLineTypeListener);
+		application.addComponentMenuElement(DrawPanelController.class, "Special line", selectLineTypeListener);
+		
 		drawerController.initialize(application.getFreePanel());
 	}
 
--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -8,6 +8,7 @@ import java.util.logging.Logger;
 import edu.kis.legacy.drawer.panel.DefaultDrawerFrame;
 import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.powp.appbase.Application;
+import edu.kis.powp.jobs2d.drivers.adapter.LineDrawerAdapter;
 import edu.kis.powp.jobs2d.drivers.adapter.MyAdapter;
 import edu.kis.powp.jobs2d.events.SelectChangeVisibleOptionListener;
 import edu.kis.powp.jobs2d.events.SelectTestFigureOptionListener;
@@ -42,6 +43,9 @@ public class TestJobs2dPatterns {
 
 		Job2dDriver testDriver = new MyAdapter();
 		DriverFeature.addDriver("Buggy Simulator", testDriver);
+		
+		Job2dDriver lineTestDriver = new LineDrawerAdapter();
+		DriverFeature.addDriver("Special line Simulator", lineTestDriver);
 
 		DriverFeature.updateDriverInfo();
 	}
