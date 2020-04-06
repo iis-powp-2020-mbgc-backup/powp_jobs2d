fd42a788aa4b248516d8e085882b866ec22ed95e
Hergoln <juliusz.malka@o2.pl>
2020-04-06 16:12:42 +0200

    Added DriverCommand, OperateToCommand and SetPositionCommand

---
+++ src/main/java/edu/kis/powp/command/OperateToCommand.java
@@ -0,0 +1,20 @@
+package edu.kis.powp.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class OperateToCommand implements DriverCommand {
+
+    private int y;
+    private int x;
+    private Job2dDriver driver;
+
+    public OperateToCommand(int y, int x, Job2dDriver driver) {
+        this.y = y;
+        this.x = x;
+        this.driver = driver;
+    }
+
+    public void execute() {
+        this.driver.operateTo(x, y);
+    }
+}
