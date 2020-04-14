8cb008f2a9e7ec2eb8044569968980b0312341d0
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-04-14 15:56:02 +0200

    Added prototypes of new shapes and introduced changes into SelectTestFigureOptionListener, from now user pass a concrete implementation of interface DriverCommand into constructor instead of switching type of figure

--- src/main/java/edu/kis/powp/jobs2d/features/FigureToDraw.java
+++ src/main/java/edu/kis/powp/jobs2d/features/FigureToDraw.java
@@ -1,5 +1,5 @@
 package edu.kis.powp.jobs2d.features;
 
 public enum FigureToDraw {
-    JOE_FIGURE_1, JOE_FIGURE_2, JANE_FIGURE
+    JOE_FIGURE_1, JOE_FIGURE_2, JANE_FIGURE, CIRCULAR_POLYGON, RECTANGLE, TRIANGLE
 }
--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.drivers.adapter.DriverAdapterOfAdapter;
 import edu.kis.powp.jobs2d.drivers.adapter.Job2dDriverAdapter;
+import edu.kis.powp.jobs2d.drivers.command.DriverCommand;
 import edu.kis.powp.jobs2d.features.FigureToDraw;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJane;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
@@ -13,33 +14,20 @@ import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 public class SelectTestFigureOptionListener implements ActionListener {
 
     private DriverManager driverManager;
-    private FigureToDraw figureToDraw;
+    private DriverCommand figureToDraw;
 
     public SelectTestFigureOptionListener(DriverManager driverManager) {
         this.driverManager = driverManager;
     }
 
-    public SelectTestFigureOptionListener(DriverManager driverManager, FigureToDraw typeOfFigure) {
+    public SelectTestFigureOptionListener(DriverManager driverManager, DriverCommand typeOfFigure) {
         this.figureToDraw = typeOfFigure;
         this.driverManager = driverManager;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        switch (figureToDraw) {
-
-            case JOE_FIGURE_1:
-                FiguresJoe.figureScript1(driverManager.getCurrentDriver());
-                break;
-
-            case JOE_FIGURE_2:
-                FiguresJoe.figureScript2(driverManager.getCurrentDriver());
-                break;
-
-            case JANE_FIGURE:
-                FiguresJane.figureScript(new DriverAdapterOfAdapter(0, 0, driverManager.getCurrentDriver()));
-                break;
-        }
-
+       figureToDraw.setDriver(driverManager.getCurrentDriver());
+       figureToDraw.execute();
     }
 }
