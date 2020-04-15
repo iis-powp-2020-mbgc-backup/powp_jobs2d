a6c5391dd6203067c927b02df5cad2c7cd882803
Graleszczur <gralewski.bartosz@gmail.com>
2020-04-15 12:54:09 +0200

    3.1.1/2 from PDF Command

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/OperateToCommand.java
@@ -0,0 +1,25 @@
+package edu.kis.powp.jobs2d.drivers.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class OperateToCommand implements DriverCommand {
+    private int y;
+    private int x;
+    private Job2dDriver driver;
+
+    public OperateToCommand(int y, int x) {
+        this.y = y;
+        this.x = x;
+    }
+
+    public void setDriver(Job2dDriver driver) {
+        this.driver = driver;
+    }
+
+    @Override
+    public void execute() {
+        if (this.driver != null) {
+            driver.setPosition(this.x, this.y);
+        }
+    }
+}
