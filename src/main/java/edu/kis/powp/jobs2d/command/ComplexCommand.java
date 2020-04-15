3463a501ea9e9af0f98c95b86545b2baeda74131
Przemysław Frąg <215719@edu.p.lodz.pl>
2020-04-15 13:22:24 +0200

    Implemented ComplexCommand class

---
+++ src/main/java/edu/kis/powp/jobs2d/command/ComplexCommand.java
@@ -0,0 +1,27 @@
+package edu.kis.powp.jobs2d.command;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComplexCommand implements DriverCommand {
+	private List<DriverCommand> commandList;
+	
+	public ComplexCommand() {
+		commandList = new ArrayList<DriverCommand>();
+	}
+	
+	public ComplexCommand(List<DriverCommand> commandList) {
+		this.commandList = commandList;
+	}
+	
+	public void add(DriverCommand command) {
+		commandList.add(command);
+	}
+
+	@Override
+	public void execute() {
+		for (DriverCommand command : commandList) {
+			command.execute();
+		}
+	}
+}
