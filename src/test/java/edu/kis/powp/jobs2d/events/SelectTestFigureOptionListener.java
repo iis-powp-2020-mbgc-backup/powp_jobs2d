d2b125059ab191b904d569cf32b0d14ee3e2ef93
Krzysztof Karasiński <krzysiek1105@opoczta.pl>
2020-04-20 20:10:48 +0200

    Resolve issues from previous laboratory submission

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -8,28 +8,36 @@ import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigureOptionListener implements ActionListener {
+    public enum FigureType {
+        FIGURE_JOE_1,
+        FIGURE_JOE_2,
+        RECTANGLE,
+        CIRCLE
+    }
 
-	private DriverManager driverManager;
+    private DriverManager driverManager;
+    private FigureType figureType;
 
-	public SelectTestFigureOptionListener(DriverManager driverManager) {
-		this.driverManager = driverManager;
-	}
+    public SelectTestFigureOptionListener(DriverManager driverManager, FigureType figureType) {
+        this.driverManager = driverManager;
+        this.figureType = figureType;
+    }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		switch(e.getActionCommand()) {
-			case "Figure Joe 1":
-				FiguresJoe.figureScript1(driverManager.getCurrentDriver());
-				break;
-			case "Figure Joe 2":
-				FiguresJoe.figureScript2(driverManager.getCurrentDriver());
-				break;
-			case "Rectangle":
-				ComplexCommandFactory.rectangle(driverManager.getCurrentDriver(), -100, 0, 100, 200).execute();
-				break;
-			case "Circle":
-				ComplexCommandFactory.circle(driverManager.getCurrentDriver(), -50, 50, 50).execute();
-				break;
-		}
-	}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (figureType) {
+            case FIGURE_JOE_1:
+                FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+                break;
+            case FIGURE_JOE_2:
+                FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+                break;
+            case RECTANGLE:
+                ComplexCommandFactory.rectangle(driverManager.getCurrentDriver(), -100, 0, 100, 200).execute();
+                break;
+            case CIRCLE:
+                ComplexCommandFactory.circle(driverManager.getCurrentDriver(), -50, 50, 50).execute();
+                break;
+        }
+    }
 }
