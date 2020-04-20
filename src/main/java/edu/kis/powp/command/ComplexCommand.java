9fcdd21c77a9ab53b831c240710c77a441dead9e
rosiak.mateusz <215866@edu.p.lodz.pl>
2020-04-20 20:33:07 +0200

    Add DriverCommand interface and some implementations

---
+++ src/main/java/edu/kis/powp/command/ComplexCommand.java
@@ -0,0 +1,17 @@
+package edu.kis.powp.command;
+
+import java.util.List;
+
+public class ComplexCommand implements DriverCommand {
+
+    private List<DriverCommand> driverCommandList;
+
+    public ComplexCommand(List<DriverCommand> driverCommandList) {
+        this.driverCommandList = driverCommandList;
+    }
+
+    @Override
+    public void execute() {
+        driverCommandList.forEach(DriverCommand::execute);
+    }
+}
