d2bdf5302eeba5281d7744642394cde39381b100
215877 <q89nTi10>
2020-04-21 13:25:26 +0200

    Added ComplexCommand

---
+++ src/main/java/edu/kis/powp/command/ComplexCommand.java
@@ -0,0 +1,22 @@
+package edu.kis.powp.command;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComplexCommand {
+    List<DriverCommand> commandList;
+
+    public ComplexCommand(){
+        commandList = new ArrayList<>();
+    }
+
+    public void addCommand(DriverCommand command){
+        commandList.add(command);
+    }
+
+    public void execute(){
+        for(DriverCommand command:commandList){
+            command.execute();
+        }
+    }
+}
