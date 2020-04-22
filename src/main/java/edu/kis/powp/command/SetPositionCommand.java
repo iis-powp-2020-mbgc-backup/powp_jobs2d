7a9792d552e29b7d04d8191fa175d05895a92609
Sebastian.Paweloszek <spaweloszek63@gmail.com>
2020-04-22 13:18:51 +0200

    DriverCommand, OperateToCommand, SetPositionCommand

---
+++ src/main/java/edu/kis/powp/command/SetPositionCommand.java
@@ -0,0 +1,21 @@
+package edu.kis.powp.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class SetPositionCommand implements DriverCommand {
+
+    private Job2dDriver job2dDriver;
+    private int x;
+    private int y;
+
+    public SetPositionCommand(Job2dDriver driver, int x, int y) {
+        this.job2dDriver = driver;
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void execute() {
+        job2dDriver.setPosition(x,y);
+    }
+}
