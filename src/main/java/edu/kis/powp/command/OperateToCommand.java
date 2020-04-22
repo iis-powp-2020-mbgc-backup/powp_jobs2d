8e34be37b0f45dd121d55af595e5e08285941747
Patryk Stępień <patryk.stepien@automotivedev.pl>
2020-04-22 12:43:10 +0200

    Added driver object to both classes

--- src/main/java/edu/kis/powp/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/command/OperateToCommand.java
@@ -1,11 +1,20 @@
 package edu.kis.powp.command;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 public class OperateToCommand implements DriverCommand {
 	
+	public OperateToCommand(int x, int y, Job2dDriver driver) {
+		this.x = x;
+		this.y = y;
+		this.driver = driver;
+	}
+	
 	public void execute() {
-		
+		this.driver.operateTo(x, y);
 	}
 	
 	private int x;
 	private int y;
+	private Job2dDriver driver;
 }
--- src/main/java/edu/kis/powp/command/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/command/SetPositionCommand.java
@@ -1,11 +1,20 @@
 package edu.kis.powp.command;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 public class SetPositionCommand implements DriverCommand {
 	
+	public SetPositionCommand(int x, int y, Job2dDriver driver) {
+		this.x = x;
+		this.y = y;
+		this.driver = driver;
+	}
+	
 	public void execute() {
-		
+		driver.setPosition(x, y);
 	}
 	
 	private int x;
 	private int y;
+	private Job2dDriver driver;
 }
