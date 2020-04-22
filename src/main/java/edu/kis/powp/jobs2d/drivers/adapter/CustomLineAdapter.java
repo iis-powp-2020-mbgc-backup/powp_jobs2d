728b6f5e801af569e84829d14ee7b8deb1746294
michaaeu <mik3wwl@gmail.com>
2020-04-22 19:19:15 +0200

    Custom Line fix, added to GUI

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/CustomLineAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/CustomLineAdapter.java
@@ -1,4 +1,24 @@
-package edu.kis.powp.jobs2d.drivers.adapter;
+package edu.kis.powp.jobs2d.drivers;
 
-public class CustomLineAdapter {
+import edu.kis.legacy.drawer.shape.line.AbstractLine;
+import edu.kis.powp.jobs2d.drivers.adapter.CustomLine;
+
+import java.awt.*;
+
+public class CustomLineAdapter extends AbstractLine implements CustomLine {
+
+    @Override
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    @Override
+    public void setDotted(boolean dotted) {
+        this.dotted = dotted;
+    }
+
+    @Override
+    public void setThickness(float thickness) {
+        this.thickness = thickness;
+    }
 }
