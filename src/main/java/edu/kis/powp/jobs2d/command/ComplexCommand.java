d414479e11b23468e97572191ce426dfcaa812c1
Kamil_Zielinski <215933@edu.p.lodz.pl>
2020-04-22 13:53:14 +0200

    Addded complexCommand class

---
+++ src/main/java/edu/kis/powp/jobs2d/command/ComplexCommand.java
@@ -0,0 +1,24 @@
+package edu.kis.powp.jobs2d.command;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComplexCommand implements DriverCommand {
+
+    List<DriverCommand> commandList = new ArrayList<>();
+
+    public ComplexCommand(ArrayList<DriverCommand> commandList) {
+        this.commandList = commandList;
+    }
+
+    @Override
+    public void execute() {
+        for (DriverCommand command : commandList) {
+            command.execute();
+        }
+    }
+
+    public void addCommand(DriverCommand command) {
+        commandList.add(command);
+    }
+}
