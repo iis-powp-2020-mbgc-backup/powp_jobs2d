1a5acd20b56feab0183cf4aeb122aa65842f3c5e
hobogalaxy <zalewski.ukas@gmail.com>
2020-04-28 03:28:13 +0200

    Make list declaration in ComplexCommand more general and private

--- src/main/java/edu/kis/powp/jobs2d/commands/ComplexCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/commands/ComplexCommand.java
@@ -3,10 +3,11 @@ package edu.kis.powp.jobs2d.commands;
 import edu.kis.powp.jobs2d.Job2dDriver;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ComplexCommand implements DriverCommand {
 
-    ArrayList<DriverCommand> commands;
+    private List<DriverCommand> commands;
 
     public ComplexCommand(){
         commands = new ArrayList<>();
