66f807e99d2ae0de0114fdaefe0206e2110deac7
Damian <Rutkowskid98@gmail.com>
2020-05-06 12:00:15 +0200

    Added test for FactoryCommand.square

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionListener;
 
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
+import edu.kis.powp.jobs2d.drivers.command.FactoryCommand;
+import edu.kis.powp.jobs2d.features.DriverFeature;
 
 public class SelectTestFigureOptionListener implements ActionListener {
 
@@ -18,8 +20,9 @@ public class SelectTestFigureOptionListener implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		if(e.getActionCommand().equals("Figure Joe 1")) {
 			FiguresJoe.figureScript1(driverManager.getCurrentDriver());
-		}else
-		{
+		}else if(e.getActionCommand().equals("Square")){
+			FactoryCommand.square(DriverFeature.getDriverManager().getCurrentDriver(),30).execute();
+		}else{
 			FiguresJoe.figureScript2(driverManager.getCurrentDriver());
 		}
 	}
